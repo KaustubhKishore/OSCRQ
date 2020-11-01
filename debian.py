c3f7a17cc09c5a8ee81d0eab13faa7192cc1a8c7
KaustubhKishore <kaustubhkishorem@gmail.com>
2020-11-02 02:48:30 +0530

    (testing) minor fix in outbound iptables

--- debian.py
+++ debian.py
@@ -2085,16 +2085,17 @@ class Debian(Helper):
             self.NotCompliant("Ensure loopback traffic is configured (Scored)")
 
     def ipoutb_3_5_4_1_3(self):
-        cmdOne = r"iptables -L -v -n"
+        # cmdOne = r"iptables -L -v -n"
         cmdTwo = r"/usr/sbin/iptables -L -v -n"
 
-        outputOne = self.caller(cmdOne)
+        # outputOne = self.caller(cmdOne)
         outputTwo = self.caller(cmdTwo)
 
         self.InfoNotSure(
             "Ensure outbound and established connections are configured (Not Scored)")
         print("Verify all rules for new outbound, and established connections match site policy:")
-        if("command not found" in outputOne):
-            print(outputTwo)
-        else:
-            print(outputOne)
+        print(outputTwo)
+        # if("command not found" in outputOne):
+            # print(outputTwo)
+        # else:
+            # print(outputOne)
