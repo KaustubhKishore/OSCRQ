3e2cd9d3882b1ee9a738b59d7b422101ca0621d9
KaustubhKishore <kaustubhkishorem@gmail.com>
2020-11-02 01:52:11 +0530

    (testing) Function caller robustness check #5

--- helper.py
+++ helper.py
@@ -57,7 +57,7 @@ class Helper:
     def caller(self, cmd):
         p = sp.Popen(cmd, shell=True,stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.STDOUT, preexec_fn = os.setsid)
         outp = p.stdout.read().decode()
-        os.killpg(os.getpgid(p.pid), signal.SIGTERM)
+        p.kill()
         return (
             outp
             # sp.Popen(cmd, shell=True, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.STDOUT)
