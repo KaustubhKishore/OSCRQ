7686fc321914a636a77d90516b1d3926ccb38f64
KaustubhKishore <kaustubhkishorem@gmail.com>
2020-11-29 02:14:14 +0530

    (Formatting) - Completed audit service discretionary access control permission modification events (4.1.9)

--- debian.py
+++ debian.py
@@ -2420,32 +2420,32 @@ class Debian(Helper):
         outputOne = self.caller(cmdTwo)
         outputTwo = self.caller(cmdThree)
 
-        if(
+        if (
                 (
-                    "-a always,exit -F arch=b32 -S chmod -S fchmod -S fchmodat -F auid>="+minUID+" -F auid!=4294967295 -k perm_mod" in outputOne and
-                    "-a always,exit -F arch=b32 -S chown -S fchown -S fchownat -S lchown -F auid>="+minUID+" -F auid!=4294967295 -k perm_mod" in outputOne and
-                    "-a always,exit -F arch=b32 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>="+minUID+" -F auid!=4294967295 -k perm_mod" in outputOne and
-                    "-a always,exit -F arch=b32 -S chmod,fchmod,fchmodat -F auid>=" + minUID + " -F auid!=-1 -F key=perm_mod" in outputTwo and
-                    "-a always,exit -F arch=b32 -S lchown,fchown,chown,fchownat -F auid>=" + minUID + " -F auid!=-1 -F key=perm_mod" in outputTwo and
-                    "-a always,exit -F arch=b32 -S setxattr,lsetxattr,fsetxattr,removexattr,lremovexattr,fremovexattr -F auid>=" + minUID + " -F auid!=-1 -F key=perm_mod" in outputTwo
+                        "-a always,exit -F arch=b32 -S chmod -S fchmod -S fchmodat -F auid>=" + minUID + " -F auid!=4294967295 -k perm_mod" in outputOne and
+                        "-a always,exit -F arch=b32 -S chown -S fchown -S fchownat -S lchown -F auid>=" + minUID + " -F auid!=4294967295 -k perm_mod" in outputOne and
+                        "-a always,exit -F arch=b32 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=" + minUID + " -F auid!=4294967295 -k perm_mod" in outputOne and
+                        "-a always,exit -F arch=b32 -S chmod,fchmod,fchmodat -F auid>=" + minUID + " -F auid!=-1 -F key=perm_mod" in outputTwo and
+                        "-a always,exit -F arch=b32 -S lchown,fchown,chown,fchownat -F auid>=" + minUID + " -F auid!=-1 -F key=perm_mod" in outputTwo and
+                        "-a always,exit -F arch=b32 -S setxattr,lsetxattr,fsetxattr,removexattr,lremovexattr,fremovexattr -F auid>=" + minUID + " -F auid!=-1 -F key=perm_mod" in outputTwo
                 ) or
                 (
-                    "-a always,exit -F arch=b64 -S chmod -S fchmod -S fchmodat -F auid>=" + minUID + " -F auid!=4294967295 -k perm_mod" in outputOne and
-                    "-a always,exit -F arch=b32 -S chmod -S fchmod -S fchmodat -F auid>=" + minUID + " -F auid!=4294967295 -k perm_mod" in outputOne and
-                    "-a always,exit -F arch=b64 -S chown -S fchown -S fchownat -S lchown -F auid>=" + minUID + " -F auid!=4294967295 -k perm_mod" in outputOne and
-                    "-a always,exit -F arch=b32 -S chown -S fchown -S fchownat -S lchown -F auid>=" + minUID + " -F auid!=4294967295 -k perm_mod" in outputOne and
-                    "-a always,exit -F arch=b64 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=" + minUID + " -F auid!=4294967295 -k perm_mod" in outputOne and
-                    "-a always,exit -F arch=b32 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=" + minUID + " -F auid!=4294967295 -k perm_mod" in outputOne and
-                    "-a always,exit -F arch=b64 -S chmod,fchmod,fchmodat -F auid>=" + minUID + " -F auid!=-1 -F key=perm_mod" in outputTwo and
-                    "-a always,exit -F arch=b32 -S chmod,fchmod,fchmodat -F auid>=" + minUID + " -F auid!=-1 -F key=perm_mod" in outputTwo and
-                    "-a always,exit -F arch=b64 -S chown,fchown,lchown,fchownat -F auid>="+minUID+" -F auid!=-1 -F key=perm_mod" and
-                    "-a always,exit -F arch=b32 -S lchown,fchown,chown,fchownat -F auid>=" + minUID + " -F auid!=-1 -F key=perm_mod" in outputTwo and
-                    "-a always,exit -F arch=b64 -S setxattr,lsetxattr,fsetxattr,removexattr,lremovexattr,fremovexattr -F auid>=" + minUID + " -F auid!=-1 -F key=perm_mod" in outputTwo and
-                    "-a always,exit -F arch=b32 -S setxattr,lsetxattr,fsetxattr,removexattr,lremovexattr,fremovexattr -F auid>=" + minUID + " -F auid!=-1 -F key=perm_mod" in outputTwo
+                        "-a always,exit -F arch=b64 -S chmod -S fchmod -S fchmodat -F auid>=" + minUID + " -F auid!=4294967295 -k perm_mod" in outputOne and
+                        "-a always,exit -F arch=b32 -S chmod -S fchmod -S fchmodat -F auid>=" + minUID + " -F auid!=4294967295 -k perm_mod" in outputOne and
+                        "-a always,exit -F arch=b64 -S chown -S fchown -S fchownat -S lchown -F auid>=" + minUID + " -F auid!=4294967295 -k perm_mod" in outputOne and
+                        "-a always,exit -F arch=b32 -S chown -S fchown -S fchownat -S lchown -F auid>=" + minUID + " -F auid!=4294967295 -k perm_mod" in outputOne and
+                        "-a always,exit -F arch=b64 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=" + minUID + " -F auid!=4294967295 -k perm_mod" in outputOne and
+                        "-a always,exit -F arch=b32 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=" + minUID + " -F auid!=4294967295 -k perm_mod" in outputOne and
+                        "-a always,exit -F arch=b64 -S chmod,fchmod,fchmodat -F auid>=" + minUID + " -F auid!=-1 -F key=perm_mod" in outputTwo and
+                        "-a always,exit -F arch=b32 -S chmod,fchmod,fchmodat -F auid>=" + minUID + " -F auid!=-1 -F key=perm_mod" in outputTwo and
+                        "-a always,exit -F arch=b64 -S chown,fchown,lchown,fchownat -F auid>=" + minUID + " -F auid!=-1 -F key=perm_mod" and
+                        "-a always,exit -F arch=b32 -S lchown,fchown,chown,fchownat -F auid>=" + minUID + " -F auid!=-1 -F key=perm_mod" in outputTwo and
+                        "-a always,exit -F arch=b64 -S setxattr,lsetxattr,fsetxattr,removexattr,lremovexattr,fremovexattr -F auid>=" + minUID + " -F auid!=-1 -F key=perm_mod" in outputTwo and
+                        "-a always,exit -F arch=b32 -S setxattr,lsetxattr,fsetxattr,removexattr,lremovexattr,fremovexattr -F auid>=" + minUID + " -F auid!=-1 -F key=perm_mod" in outputTwo
 
                 )
         ):
             self.Compliant("Ensure discretionary access control permission modification events are collected (Scored)")
         else:
-            self.NotCompliant("Ensure discretionary access control permission modification events are collected (Scored)")
-
+            self.NotCompliant(
+                "Ensure discretionary access control permission modification events are collected (Scored)")
