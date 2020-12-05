604de6306862bfc0c481196db78c7f5116ccb96e
KaustubhKishore <kaustubhkishorem@gmail.com>
2020-12-05 11:31:30 +0530

    (Fix) + recordID check #2 + Emoji change to fix formatting check

--- debian.py
+++ debian.py
@@ -246,7 +246,8 @@ class Debian(Helper):
                 "Info Manual": self.INFONOTSURE
             }
 
-            self.recordID._id = collection.insert_one(benchmark)
+            temp = collection.insert_one(benchmark)
+            self.recordID = temp._id
             print("Pushed Successful: ", self.recordID)
         except Exception as e:
             print("Failure: ",e)
