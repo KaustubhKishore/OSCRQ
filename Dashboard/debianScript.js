dcb2bb848a938b2aefac2d64ccaca5d4b0e6a362
vaibhsri <vsv1998@gmail.com>
2020-12-06 15:22:26 +0530

    Dashboard final

--- Dashboard/debianScript.js
+++ Dashboard/debianScript.js
@@ -1,5 +1,5 @@
 var req = new XMLHttpRequest();
-req.open('GET', 'http://localhost:3000/windowsdata');
+req.open('GET', 'http://localhost:3000/debiandata');
 req.onload = function(){
     var x = JSON.parse(req.response);
     var data = document.getElementById("data");
@@ -8,7 +8,7 @@ req.onload = function(){
     var sideDevice = document.getElementById("sideDevice");
     var detailsAnalysis = document.getElementById("detailsAnalysis");
     for( i=0; i<x.length; i++){
-        var dateString = x[i]["TestTime"]; 
+        var dateString = x[i]["BenchmarkingTime"]; 
         var dateParts = dateString.split("/");
         var TempTime = new Date(dateParts[1]+'/'+dateParts[0]+'/'+dateParts[2]); 
         if(i==0){
@@ -26,8 +26,8 @@ req.onload = function(){
         console.log('Test Times' + TempTime + " " + LatestTime + " " + LatestIndex);
     }
     var totalComp = x[LatestIndex]["Compliant"].length;
-    var totalNonComp = x[LatestIndex]["Non Compliant"].length;
-    var testTime = x[LatestIndex]["TestTime"];
+    var totalNonComp = x[LatestIndex]["Not Compliant"].length;
+    var testTime = x[LatestIndex]["BenchmarkingTime"];
     var platform = x[LatestIndex]["Platform"];
     var devid = x[LatestIndex]["DeviceID"];
     var total = totalComp + totalNonComp;
@@ -52,12 +52,16 @@ req.onload = function(){
         detailsAnalysis.innerHTML += "<tr><td>" + x[LatestIndex]["Compliant"][i] + "</td><td><span class='compliant'>Compliant</span><br></td></tr>";
     }
     for(i=0 ; i<totalNonComp ; i++){
-        if (x[LatestIndex]["Not Configured"].includes(x[LatestIndex]["Non Compliant"][i])){
-            detailsAnalysis.innerHTML += "<tr><td>" + x[LatestIndex]["Non Compliant"][i] + "</td><td><span class='notconfigured'>NotConfigured</span> <br> <span class='noncompliant'>NonCompliant</span><br></td></tr>";
-        }
-        else{
-            detailsAnalysis.innerHTML += "<tr><td>" + x[LatestIndex]["Non Compliant"][i] + "</td><td><span class='noncompliant'>NonCompliant</span><br></td></tr>";
-        }
+        detailsAnalysis.innerHTML += "<tr><td>" + x[LatestIndex]["Not Compliant"][i] + "</td><td><span class='noncompliant'>NonCompliant</span><br></td></tr>";
+    }
+    for(i=0 ; i<x[LatestIndex]["Info Compliant"].length ; i++){
+        detailsAnalysis.innerHTML += "<tr><td>" + x[LatestIndex]["Info Compliant"][i] + "</td><td><span class='notconfigured'>InfoCompliant</span><br></td></tr>";
+    }
+    for(i=0 ; i<x[LatestIndex]["Info NotCompliant"].length ; i++){
+        detailsAnalysis.innerHTML += "<tr><td>" + x[LatestIndex]["Info NotCompliant"][i] + "</td><td><span class='notconfigured'>InfoNotCompliant</span><br></td></tr>";
+    }
+    for(i=0 ; i<x[LatestIndex]["Info Manual"].length ; i++){
+        detailsAnalysis.innerHTML += "<tr><td>" + x[LatestIndex]["Info Manual"][i] + "</td><td><span class='notconfigured'>InfoManual</span><br></td></tr>";
     }
     console.log(x);
 }
@@ -69,8 +73,8 @@ function changeIndex(LatestIndex){
     var sideDevice = document.getElementById("sideDevice");
     var detailsAnalysis = document.getElementById("detailsAnalysis");
     var totalComp = x[LatestIndex]["Compliant"].length;
-    var totalNonComp = x[LatestIndex]["Non Compliant"].length;
-    var testTime = x[LatestIndex]["TestTime"];
+    var totalNonComp = x[LatestIndex]["Not Compliant"].length;
+    var testTime = x[LatestIndex]["BenchmarkingTime"];
     var platform = x[LatestIndex]["Platform"];
     var devid = x[LatestIndex]["DeviceID"];
     var total = totalComp + totalNonComp;
@@ -98,12 +102,16 @@ function changeIndex(LatestIndex){
         detailsAnalysis.innerHTML += "<tr><td>" + x[LatestIndex]["Compliant"][i] + "</td><td><span class='compliant'>Compliant</span><br></td></tr>";
     }
     for(i=0 ; i<totalNonComp ; i++){
-        if (x[LatestIndex]["Not Configured"].includes(x[LatestIndex]["Non Compliant"][i])){
-            detailsAnalysis.innerHTML += "<tr><td>" + x[LatestIndex]["Non Compliant"][i] + "</td><td><span class='notconfigured'>NotConfigured</span> <br> <span class='noncompliant'>NonCompliant</span><br></td></tr>";
-        }
-        else{
-            detailsAnalysis.innerHTML += "<tr><td>" + x[LatestIndex]["Non Compliant"][i] + "</td><td><span class='noncompliant'>NonCompliant</span><br></td></tr>";
-        }
+        detailsAnalysis.innerHTML += "<tr><td>" + x[LatestIndex]["Not Compliant"][i] + "</td><td><span class='noncompliant'>NonCompliant</span><br></td></tr>";
+    }
+    for(i=0 ; i<x[LatestIndex]["Info Compliant"].length ; i++){
+        detailsAnalysis.innerHTML += "<tr><td>" + x[LatestIndex]["Info Compliant"][i] + "</td><td><span class='notconfigured'>InfoCompliant</span><br></td></tr>";
+    }
+    for(i=0 ; i<x[LatestIndex]["Info NotCompliant"].length ; i++){
+        detailsAnalysis.innerHTML += "<tr><td>" + x[LatestIndex]["Info NotCompliant"][i] + "</td><td><span class='notconfigured'>InfoNotCompliant</span><br></td></tr>";
+    }
+    for(i=0 ; i<x[LatestIndex]["Info Manual"].length ; i++){
+        detailsAnalysis.innerHTML += "<tr><td>" + x[LatestIndex]["Info Manual"][i] + "</td><td><span class='notconfigured'>InfoManual</span><br></td></tr>";
     }
     console.log(x);
 }
