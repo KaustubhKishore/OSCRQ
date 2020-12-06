1514c94a2be4827a11ca7bf5acb7a1de5f4faf90
KaustubhKishore <kaustubhkishorem@gmail.com>
2020-12-06 15:57:44 +0530

    Intentional build fail - Email Test #4

--- Linux/helper.py
+++ Linux/helper.py
@@ -11,7 +11,7 @@ class Helper:
     info = "\033[94m" + "[INFO]" + "\033[0m" + " "
 
     def __init__(self):
-        self.id = "Jenkins Test Push - Intentional fail #3"
+        self.id = "Jenkins Test Push - Intentional fail #4"
         self.platform = ""
         self.dateAndTime = ""
         self.recordID = ""
